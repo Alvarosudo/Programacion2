51d44422a9f6129150e7547a1c1e5ec30f17d7eb
root <root@Alvaro>
2025-04-01 21:25:08 +0200

    Grafos

--- Practica2/irp2-p2/graph.cc
+++ Practica2/irp2-p2/graph.cc
@@ -1,1 +1,12 @@
 //graph.cc
+#include "graph.h"
+
+GraphPtr    graphCreate      (uint64_t nv, GraphType directed = GraphType::DIRECTED){
+    GraphPtr grafo= new Graph;
+    grafo->nv = nv;
+    grafo->gt = directed;
+    grafo->ne = nv;
+    grafo->v= nullptr;
+
+    return grafo;
+}
